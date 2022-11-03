5b0c60b47f74c2b6a91c1923be54cc0934a7a219
eiiti <eiiti@mail.ru>
2022-11-04 01:59:32 +0300

    initial commit

---
+++ Homework1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+int min = number2;
+
+if(number1>number2) {
+    max = number1; 
+    min = number2;
+}
+
+if(number2>number1) {
+    max = number2; 
+    min = number1;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
---
+++ Homework4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int iter = 1;
+
+while (iter<=n)
+{
+    if(iter%2 == 0) {
+        Console.WriteLine(iter);
+    }
+    iter++;
+}
